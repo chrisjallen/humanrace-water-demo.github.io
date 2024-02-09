00c338cf8d7f941019f791fddab73b9c9468ccb0
Allen, Christopher (UK London) <christopher.allen4@baesystems.com>
2024-02-09 18:19:46 +0000

    publish password

--- src/App.tsx
+++ src/App.tsx
@@ -1,52 +1,85 @@
 // import { useState } from "react";
-import { useEffect, useLayoutEffect } from "react";
+import { FormEventHandler, useEffect, useLayoutEffect } from "react";
 import "./App.css";
 import "./scene/ripples/ripples";
 import RipplesScene from "./scene/scene";
-// import pharLogo from "./assets/evian-phar-logo.png";
-import testImageP from "./assets/ZGhertner_PW_1120_1164-09_v2.jpg";
+
+const PASSWORD = "evianPharrellW@ter";
 
 function App() {
-  // const [count, setCount] = useState(0);
+  const { access, attemptLogin } = useSec();
   useLayoutEffect(() => {
-    const rippleScene = new RipplesScene({
-      viscosity: 15,
-      speed: 15,
-      size: 1.25,
+    if (access) {
+      new RipplesScene({
+        viscosity: 15,
+        speed: 15,
+        size: 1.25,
 
-      displacementStrength: 5,
-      lightIntensity: 0.1,
-      shadowIntensity: 0.1
-    });
-  }, []);
+        displacementStrength: 5,
+        lightIntensity: 0.1,
+        shadowIntensity: 0.1
+      });
+    }
+  }, [access]);
 
   return (
     <>
-      <div id="canvas"></div>
-      <header>
-        <div className="humanracelogo"></div>
-        <div className="collab"></div>
-        <div className="evianlogo"></div>
-      </header>
-
-      <section id="content">
-        <div id="canvas"></div>
-
-        <div id="water-ripples">
-          <div id="water-ripples-title">
-            <h1>Ripples</h1>
-          </div>
-
-          <img
-            src="./assets/ZGhertner_PW_1120_1164-09_v2.jpg"
-            alt="Photo by David Pisnoy on Unsplash"
-            crossOrigin="anonymous"
-            data-sampler="planeTexture"
-          />
-        </div>
-      </section>
+      {!access && (
+        <section className="auth">
+          <form action="" onSubmit={attemptLogin}>
+            <label htmlFor="password">Enter password:</label>
+            <input type="text" name="password" id="password" />
+          </form>
+        </section>
+      )}
+      {access && (
+        <>
+          <main>
+            <div id="canvas"></div>
+            <header>
+              <div className="humanracelogo"></div>
+              <div className="collab"></div>
+              <div className="evianlogo"></div>
+            </header>
+            <section id="content">
+              <div id="canvas"></div>
+              <div id="water-ripples">
+                <div id="water-ripples-title">
+                  <h1>Ripples</h1>
+                </div>
+                <img
+                  src="./assets/ZGhertner_PW_1120_1164-09_v2.jpg"
+                  alt="Photo by David Pisnoy on Unsplash"
+                  crossOrigin="anonymous"
+                  data-sampler="planeTexture"
+                />
+              </div>
+            </section>
+          </main>
+        </>
+      )}
     </>
   );
 }
+
+const useSec = () => {
+  const access = localStorage.getItem("access");
+  if (!access) {
+    const attemptLogin: FormEventHandler<HTMLFormElement> = (event) => {
+      const password =
+        event.currentTarget.querySelector<HTMLInputElement>("#password")?.value;
+      if (password === PASSWORD) {
+        localStorage.setItem("access", "true");
+      }
+    };
+    return {
+      access: false,
+      attemptLogin
+    };
+  }
+  return {
+    access: true
+  };
+};
 
 export default App;
